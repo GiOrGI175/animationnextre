f77c5b251461f966eecb4271679194640d8317a7
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-11-08 19:59:51 +0400

    core/structure

---
+++ app/constants/card.ts
@@ -0,0 +1,38 @@
+export type CardTupe = {
+  id: number;
+  title: string;
+  image: string;
+};
+
+export const cards: CardTupe[] = [
+  {
+    id: 1,
+    title: 'title1',
+    image: 'assets/images/image1.png',
+  },
+  {
+    id: 2,
+    title: 'title2',
+    image: 'assets/images/image2.png',
+  },
+  {
+    id: 3,
+    title: 'title3',
+    image: 'assets/images/image3.png',
+  },
+  {
+    id: 4,
+    title: 'title4',
+    image: 'assets/images/image4.png',
+  },
+  {
+    id: 5,
+    title: 'title5',
+    image: 'assets/images/image5.png',
+  },
+  {
+    id: 6,
+    title: 'title6',
+    image: 'assets/images/image6.png',
+  },
+];
