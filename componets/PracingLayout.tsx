7c76dade9182db4e7b89406b04e794cca77cf4b2
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-11-11 21:55:49 +0400

    feature/animation popup

--- componets/PracingLayout.tsx
+++ componets/PracingLayout.tsx
@@ -1,5 +1,7 @@
 'use client';
 import { ReactElement, useState } from 'react';
+import Link from 'next/link';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function PracingLayout({
   children,
@@ -10,15 +12,35 @@ export default function PracingLayout({
   href: string;
   MyContentlayout?: React.ElementType;
 }) {
-  const [open, estOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const ShowFlayout = MyContentlayout && open;
 
   return (
-    <div>
+    <div
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className='relative text-white'
+    >
+      <Link href={href}>{children}</Link>
+      {ShowFlayout && (
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 15 }}
+            style={{ transform: '-50%' }}
+            transition={{ duration: 0.3, ease: 'easeOut' }}
+            className='absolute text-black bg-white left-1/2 top-10'
+          >
+            <div className='absolute top-0 w-4 h-4 rotate-45 -translate-x-1/2 -translate-y-1/2 bg-white left-1/2' />
+            {MyContentlayout && <MyContentlayout />}
+          </motion.div>
+        </AnimatePresence>
+      )}
+
       <p>header</p>
-      {children}
-      {MyContentlayout && <MyContentlayout />}
+
       <p>footer</p>
     </div>
   );
